58888906078791e55b093686f97034ce1d93d018
lordoferos <33201932+lordoferos@users.noreply.github.com>
2020-08-14 11:20:17 +0300

    formatted bulk email

--- bulkemail.R
+++ bulkemail.R
@@ -1,18 +1,21 @@
 # Install email rep
 install.packages("emailrep", repos = "https://cinc.rud.is")
+
 # load emailrep
 library(emailrep)
+
 ## Check the validity of emails
 email_rep("tim@apple.com")
 email_rep("alfylev@gmail.com")
 email_rep("reginasmith2424@gmail.com")
 email_rep(c("aggiemaida@gmail.com", 
     "chevviesq@gmail.com"))
+
 ## Multiple IDs
 email_ids <- c("info@jabberbomb.com", 
     "bensonjoyce39@gmail.com", "channing@indiehackers.com")
-# use purrr for a bit of functional
-# programming (with map())
+
+# use purrr for a bit of functional programming (with map())
 library(purrr)
 library(magrittr)
 result_df <- map(email_ids, email_rep) %>% 
