98f57d2b129c26e5740bec374b44ad4190d54497
lordoferos <33201932+lordoferos@users.noreply.github.com>
2020-08-14 11:40:08 +0300

    Update web_scaping.R

--- web_scaping.R
+++ web_scaping.R
@@ -1,5 +1,7 @@
 library(Rcrawler)
 library(foreach)
-Rcrawler(Website = "https://www.twitter.com", 
-    no_cores = 4, no_conn = 4, KeywordsFilter = c("missing person"), 
+
+# Probably won't work for sites that need API
+Rcrawler(Website = "https://www.dailyupdates.co.ke", 
+    no_cores = 4, no_conn = 4, KeywordsFilter = c("missing"), 
     RequestsDelay = 3)
