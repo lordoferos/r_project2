6d6b54e6eef37834b2fa80c9e7e8358178cf2743
lordoferos <33201932+lordoferos@users.noreply.github.com>
2020-08-16 12:45:00 +0300

    Update web_scaping.R

--- web_scaping.R
+++ web_scaping.R
@@ -5,3 +5,8 @@ library(foreach)
 Rcrawler(Website = "https://www.dailyupdates.co.ke", 
     no_cores = 4, no_conn = 4, KeywordsFilter = c("missing"), 
     RequestsDelay = 3)
+
+
+Rcrawler(Website = "https://www.tuko.co.ke", 
+    no_cores = 4, no_conn = 4, KeywordsFilter = c("kenyatta"), 
+    RequestsDelay = 3)
